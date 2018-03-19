9c209542e6b8fad0dc4ee6531e17eaabc2dbe30d
ueki05 <uekiyuta1991@gmail.com>
2018-03-20 01:41:49 +0900

    listの表示項目やページネーション、formのバリデーターを設定

--- lib/form/doctrine/UserForm.class.php
+++ lib/form/doctrine/UserForm.class.php
@@ -13,5 +13,25 @@ class UserForm extends BaseUserForm
   public function configure()
   {
     $this->useFields(array('first_name', 'last_name', 'email'));
+
+    $this->setValidator('first_name', new sfValidatorString(array(
+      'min_length' => 1,
+      'max_length' => 50,
+    ),
+    array(
+      'min_length' => 'Please post a longer message',
+      'max_length' => 'Please be less verbose',
+    )));
+
+    $this->setValidator('last_name', new sfValidatorString(array(
+      'min_length' => 1,
+      'max_length' => 50,
+    ),
+    array(
+      'min_length' => 'Please post a longer message',
+      'max_length' => 'Please be less verbose',
+    )));
+
+    $this->setValidator('email', new sfValidatorEmail());
   }
 }
